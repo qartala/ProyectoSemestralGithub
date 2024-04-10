7c820da1737d66bf73c44e4fa07cce101308a9e1
Mr-P1 <jo.salvo@duocuc.cl>
2024-04-10 00:24:04 -0400

    Inicio proyecto
    Se inicio el proyecto en django

---
+++ casoSemestral/modulos/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('login/', views.iniciar_sesion,name="iniciar_sesion"),
+    path('logup/',views.registrarse,name='registrarse'),
+    path('logout/',views.cerrar_sesion,name='cerrar_sesion')
+]
---
+++ casoSemestral/modulos/myapp/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User 
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+
+def registrarse(request):
+    if request.method == 'GET':
+
+        return render(request,'login/registrarse.html')
+    
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                usuario= User.objects.create_user(username= request.POST['username'], password= request.POST['password1'],email=request.POST['email'])
+                usuario.save()
+
+                return redirect('iniciar_sesion')
+
+            except:
+                datos = {
+                    'error':"El usuario ya existe"
+                }
+                return render(request,'login/registrarse.html',datos)
+        
+        else:
+            
+            datos = {
+                'error': "Las contraseñas deben ser las mismas"
+            }
+
+            return render(request,'login/registrarse.html',datos)
+
+
+
+def iniciar_sesion(request):
+    if request.method == 'GET':
+        return render(request, 'login/iniciar_sesion.html')
+    
+    else:
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+
+        if user is None :
+            #No se encontro al usuario
+            datos = {
+                "error":'No se encontro al usuario'
+            }
+
+            return render(request,'login/iniciar_sesion.html',datos)
+        
+        else:
+            login(request,user)
+            return redirect('index')
+        
+
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('index')
